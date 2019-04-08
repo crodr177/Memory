3ff1ea1fa7c9286e82b46e54de86916160661cf1
Cruz Rodriguez <cruzrodriguez1127@gmail.com>
2019-04-08 08:26:39 -0700

    final edit

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
 let card = document.getElementsByClassName("card");
+let score = document.querySelector("#score");
 let cards = [];
-console.log(cards);
 let deck = document.getElementById("card-deck");
 let counter = document.querySelector(".moves");
-let matchedCard = document.getElementsByClassName("match");
-let openedCards = [];
+let openCardClass1 = '';
+let openCardClass2 = '';
+let cardholder = '';
+let openedCards1 = [];
+let openedCards2 = [];
+let cardholderarr = [];
+let stoparr = [];
+let lives = 10;
 
+//shuffles the cards everytime the browser is loaded
 function shuffle(array) {    
   let currentIndex = array.length, temporaryValue, randomIndex;
 
@@ -19,10 +26,100 @@ function shuffle(array) {
   return array;
 };
 
+// makes a new array that is pushed to cards so we know how many cards are being played
+for (var i = 0; i < card.length; i++){
+  cards.push(card[i]);
+};
+
+// when cards match
+function matched(){
+  openCardClass1.addClass("show").addClass("disabled");
+  openCardClass2.addClass("show").addClass("disabled");
+  openCardClass1 = cardholder.addClass("show").addClass("disabled");
+  openedCards1 = cardholderarr;
+  openedCards2 = [];
+  cardholderarr = [];
+  openCardClass2 = '';
+  cardholder = '';
+  stoparr.push("matched");
+}
+
+// when cards don't match
+function unmatched(){
+ openCardClass1.removeClass("show").removeClass("disabled");
+ openCardClass2.removeClass("show").removeClass("disabled");
+ cardholder.removeClass("show");
+  openedCards1 = [];
+  openedCards2 = [];
+  cardholderarr = [];
+  openCardClass1 = '';
+  openCardClass2 = '';
+  cardholder = '';
+}
+
+
+//compares the two cards opened
+function cardOpen(){
+    if(openedCards1[0] === openedCards2[0]){
+        matched();
+    } else {
+      unmatched();
+    }
+}
+
+//click function to assign each click a variable to compare
+$(".card").on("click", function(){
+  
+  if(openCardClass1 === ''){
+  openCardClass1 = $(this).addClass("show").addClass("disabled");
+  console.log(openCardClass1);
+  }
+  else if(openCardClass2 === '') {
+    openCardClass2 = $(this).addClass("show").addClass("disabled");
+    console.log(openCardClass2);
+  }
+  else {
+    cardholder = $(this).addClass("show");
+    console.log(cardholder);
+  }
+
+  if (openedCards1.length === 0) {
+    var classname = this.className;
+    openedCards1.push(classname);
+    console.log(openedCards1);
+  }
+  else if(openedCards2.length === 0){
+    var classname = this.className;
+    openedCards2.push(classname);
+    console.log(openedCards2);
+  }
+  else {
+    var classname = this.className;
+    cardholderarr.push(classname);
+    console.log(cardholderarr);
+  }
+
+  if(stoparr.length === 8) {
+    alert("you won");
+    location.reload();
+  }
+
+  if(cardholderarr.length === 1) {
+    cardOpen();
+    lives--;
+    score.innerHTML = `Turns: ${lives}`;
+    if(lives === 0) {
+      alert("You ran out of turns");
+      location.reload();
+    }
+  }
+})
+
 document.body.onload = startGame();
 
 function startGame(){
   cards = shuffle(cards);
+  score.innerHTML = `Turns: ${lives}`;
 
   // Render shuffled cards on to Viewport
   for (let i = 0; i < cards.length; i++){
@@ -30,74 +127,5 @@ function startGame(){
     [].forEach.call(cards, function(item) {
         deck.appendChild(item);
     });
-    cards[i].classList.remove("show", "open", "match", "disabled");
   }
 }
-
-// toggles open and show class to display cards
-let displayCard = function (){
-  this.classList.toggle("open");
-  this.classList.toggle("show");
-  this.classList.toggle("disabled");
-};
-
-// add opened cards to OpenedCards list and check if cards are match or not
-function cardOpen() {
-  openedCards.push(this);
-  let len = openedCards.length;
-  if(len === 2){
-    moveCounter();
-    if(openedCards[0].type === openedCards[1].type){
-        matched();
-    } else {
-        unmatched();
-    }
-  }
-};
-
-// when cards match
-function matched(){
-  openedCards[0].classList.add("match", "disabled");
-  openedCards[1].classList.add("match", "disabled");
-  openedCards[0].classList.remove("show", "open", "no-event");
-  openedCards[1].classList.remove("show", "open", "no-event");
-  openedCards = [];
-}
-
-// when cards don't match
-function unmatched(){
-  openedCards[0].classList.add("unmatched");
-  openedCards[1].classList.add("unmatched");
-  disable();
-  setTimeout(function(){
-    openedCards[0].classList.remove("show", "open", "no-event","unmatched");
-    openedCards[1].classList.remove("show", "open", "no-event","unmatched");
-    enable();
-    openedCards = [];
-  },1000);
-}
-
-// disable cards
-function disable(){
-  Array.prototype.filter.call(cards, function(card){
-    card.classList.add('disabled');
-  });
-}
-
-// enable cards and disable matched cards
-function enable(){
-  Array.prototype.filter.call(cards, function(card){
-    card.classList.remove('disabled');
-    for(var i = 0; i < matchedCard.length; i++){
-      matchedCard[i].classList.add("disabled");
-    }
-  });
-}
-
-// loop to add event listeners to each card
-for (var i = 0; i < cards.length; i++){
-  card = cards[i];
-  card.addEventListener("click", displayCard);
-  card.addEventListener("click", cardOpen);
-  card.addEventListener("click",congratulations);
-};
